f9224988c6b8ba8a000578ce759017fdb126e887
Tomilo Artem <artsiom.tamila@gmail.com>
2022-10-27 16:16:21 +0300

    Initial commit

---
+++ Test_task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_task
+//
+//  Created by Артем Томило on 27.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
